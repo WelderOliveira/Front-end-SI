6ad0e942c30fd2a0e1023fb807d0ce911eb981f8
mateuspds <mateus.silva09082000@gmail.com>
2023-01-23 12:37:59 -0300

    vou comecar os formularios

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,8 +10,8 @@
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-item nav-link active" href="/home">Home</a>
-        <a class="nav-item nav-link" href="/newsletter">NewsLetter</a>
-        <a class="nav-item nav-link" href="#">Pricing</a>
+        <a class="nav-item nav-link" href="/newsletter">Login</a>
+        <a class="nav-item nav-link" href="#">Quem Somos?</a>
         <a class="nav-item nav-link disabled" href="#">Disabled</a>
       </div>
     </div>
